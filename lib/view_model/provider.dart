8445e784b1f100081600643e9f85a0d81f9d0813
aldyrizkyk <aldyrizkyk@gmail.com>
2022-11-07 15:05:31 +0700

    Mengedit provider

--- lib/view_model/provider.dart
+++ lib/view_model/provider.dart
@@ -3,22 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Plant with ChangeNotifier {
-  late SharedPreferences registerdata;
   List<PlantModel> _plants = [];
   List<PlantModel> get plants => _plants;
 
-  void addPlant(PlantModel contact) async {
-    registerdata = await SharedPreferences.getInstance();
-    _plants =
-        (registerdata.getStringList('addplant') ?? true) as List<PlantModel>;
-
+  void addPlant(PlantModel contact) {
     _plants.add(contact);
     notifyListeners();
   }
 
-  void deletePlant(int index) async {
-    registerdata = await SharedPreferences.getInstance();
-    _plants = (registerdata.remove('plant')) as List<PlantModel>;
+  void deletePlant(int index) {
     _plants.removeAt(index);
     notifyListeners();
   }
